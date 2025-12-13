dbfcafed7ed5a1c83328ccaec8c2c0b3a66d607b
rudyanta.alfeus@gmail.com <rudyanta.alfeus@gmail.com>
2025-12-13 21:03:24 +0700

    Assignment completed

--- src/challenge1.ts
+++ src/challenge1.ts
@@ -1,2 +1,48 @@
+type Book = {
+  title: string;
+  author: string;
+  publicationYear: number;
+};
+
+const books: Book[] = [];
+
+const printBook = (book: Book): void => {
+  console.log(`- ${book.title} by ${book.author} (${book.publicationYear})`);
+};
+
+const addBook = (
+  title: string,
+  author: string,
+  publicationYear: number
+): void => {
+  books.push({ title, author, publicationYear });
+  console.log(`Book added: "${title}" by ${author} (${publicationYear})`);
+};
+
+const listBooks = (): void => {
+  console.log('All Books:');
+
+  books.forEach(printBook);
+};
+
+const searchBook = (title?: string): void => {
+  if (!title) {
+    console.log('Please provide a title to search.');
+    return;
+  }
+
+  const filteredBooks: Book[] = books.filter((book: Book): boolean =>
+    book.title.includes(title)
+  );
+
+  if (filteredBooks.length === 0) {
+    console.log(`No books found with title containing "${title}".`);
+    return;
+  }
+
+  console.log(`Search Results for "${title}":`);
+  filteredBooks.forEach(printBook);
+};
+
 // Don't delete code bellow and this code must be at the bottom of the file
 export { addBook, listBooks, searchBook };
